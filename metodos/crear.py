04d4d81facc9b80b5167d081e734b93359172e7e
Jimmer942 <942@holbertonschool.com>
2020-02-08 13:49:37 -0500

    crear.py [Fix: errors about the tables]

--- metodos/crear.py
+++ metodos/crear.py
@@ -14,12 +14,13 @@ def crear_ac_invoice_items(ID, TENANT_ID, PRODUCT_ID, INVOICE_ID, QUANTITY, UNIT
                                                            QUANTITY,
                                                            UNIT_VALUE,
                                                            ITEM_VALUE)
-    cursor.execute(command + values)
+    command += values
+    cursor.execute(command)
     db.commit()
 
 def crear_ac_invoices(ID, TENANT_ID, CUSTOMER_ID, DOC_DATE, DOC_NUMBER, TOTAL_DISCOUNT, TOTAL_TAX, TOTAL_VALUE):
     command = "INSERT INTO ac_invoices(ID, TENANT_ID, CUSTOMER_ID, DOC_DATE, DOC_NUMBER, TOTAL_DISCOUNT, TOTAL_TAX, TOTAL_VALUE) "
-    values = "VALUES ({}, {}, {}, {}, {}, {}, {}, {});".format(ID,
+    values = "VALUES ({}, {}, {}, '{}', '{}', {}, {}, {});".format(ID,
                                                                TENANT_ID,
                                                                CUSTOMER_ID,
                                                                DOC_DATE,
@@ -27,26 +28,31 @@ def crear_ac_invoices(ID, TENANT_ID, CUSTOMER_ID, DOC_DATE, DOC_NUMBER, TOTAL_DI
                                                                TOTAL_DISCOUNT,
                                                                TOTAL_TAX,
                                                                TOTAL_VALUE)
-    cursor.execute(command + values)
+    command += values
+    cursor.execute(command)
     db.commit()
 
 
 
 def crear_ac_products(ID, TENANT_ID, NAME, DESCRIPTION, LIST_PRICE):
     command = "INSERT INTO ac_products(ID, TENANT_ID, NAME, DESCRIPTION, LIST_PRICE) "
-    values = "VALUES ({}, {}, {}, {}, {});".format(ID, TENANT_ID, NAME, DESCRIPTION, LIST_PRICE)
-    cursor.execute(command + values)
+    values = "VALUES ({}, {}, '{}', '{}', {});".format(ID, TENANT_ID, NAME, DESCRIPTION, LIST_PRICE)
+    command += values
+    cursor.execute(command)
     db.commit()
 
 
 def crear_ac_tenant(ID, NAME):
     command = "INSERT INTO ac_tenant(ID, NAME) "
-    values = "VALUES ({}, {});".format(ID, NAME)
-    cursor.execute(command + values)
+    values = "VALUES ({}, '{}');".format(ID, NAME)
+    command += values
+    cursor.execute(command)
     db.commit()
 
 def crear_customer(ID, TENANT_ID, FIRST_NAME, LAST_NAME):
     command = "INSERT INTO customer(ID, TENANT_ID, FIRST_NAME, LAST_NAME)) "
-    values = "VALUES ({}, {}, {}, {});".format(ID, TENANT_ID, FIRST_NAME, LAST_NAME))
-    cursor.execute(command + values)
+    values = "VALUES ({}, {}, '{}', '{}');".format(ID, TENANT_ID, FIRST_NAME, LAST_NAME)
+    command += values
+    cursor.execute(command)
     db.commit()
+    db.close()
