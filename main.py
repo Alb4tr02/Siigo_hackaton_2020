9411a78103d0ad5e5a27d73931d1d5c0f4ce5efc
Jimmer942 <942@holbertonschool.com>
2020-02-08 13:19:43 -0500

    main.py [Fix: import the delete function]

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
 
-from metodos.lista import listar
-from metodos.crear import crear
-
-
-listar()
-print()
-
-crear()
-
-listar()
+from metodos.lista import *
+from metodos.crear import *
+from metodos.delete import *
