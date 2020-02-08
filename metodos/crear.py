dc27c3c2cbd8968f3e0142bfbbbe90881bf56fcd
Jimmer942 <942@holbertonschool.com>
2020-02-08 13:05:28 -0500

    crear.py [Update: se creo una funcion para cada tabla con sus respectivos parametros dentro de la base de datos siigo]

--- metodos/crear.py
+++ metodos/crear.py
@@ -2,11 +2,51 @@
 
 import MySQLdb
 
-def crear():
-    db = MySQLdb.connect(host="localhost", user="root",passwd="k@m1l0her196", db="prueba")
-    cursor = db.cursor()
+db = MySQLdb.connect(host="localhost", user="electros", passwd="electros", db="siigo")
+cursor = db.cursor()
 
-    command = "INSERT INTO second_table(ID, NAME, SCORE) VALUES ('6', 'Jimmer', 17);"
-    cursor.execute(command)
+def crear_ac_invoice_items(ID, TENANT_ID, PRODUCT_ID, INVOICE_ID, QUANTITY, UNIT_VALUE, ITEM_VALUE):
+    command = "INSERT INTO ac_invoice_items(ID, TENANT_ID, PRODUCT_ID, INVOICE_ID, QUANTITY, UNIT_VALUE, ITEM_VALUE) "
+    values = "VALUES ({}, {}, {}, {}, {}, {}, {});".format(ID,
+                                                           TENANT_ID,
+                                                           PRODUCT_ID,
+                                                           INVOICE_ID,
+                                                           QUANTITY,
+                                                           UNIT_VALUE,
+                                                           ITEM_VALUE)
+    cursor.execute(command + values)
     db.commit()
-    db.close()
+
+def crear_ac_invoices(ID, TENANT_ID, CUSTOMER_ID, DOC_DATE, DOC_NUMBER, TOTAL_DISCOUNT, TOTAL_TAX, TOTAL_VALUE):
+    command = "INSERT INTO ac_invoices(ID, TENANT_ID, CUSTOMER_ID, DOC_DATE, DOC_NUMBER, TOTAL_DISCOUNT, TOTAL_TAX, TOTAL_VALUE) "
+    values = "VALUES ({}, {}, {}, {}, {}, {}, {}, {});".format(ID,
+                                                               TENANT_ID,
+                                                               CUSTOMER_ID,
+                                                               DOC_DATE,
+                                                               DOC_NUMBER,
+                                                               TOTAL_DISCOUNT,
+                                                               TOTAL_TAX,
+                                                               TOTAL_VALUE)
+    cursor.execute(command + values)
+    db.commit()
+
+
+
+def crear_ac_products(ID, TENANT_ID, NAME, DESCRIPTION, LIST_PRICE):
+    command = "INSERT INTO ac_products(ID, TENANT_ID, NAME, DESCRIPTION, LIST_PRICE) "
+    values = "VALUES ({}, {}, {}, {}, {});".format(ID, TENANT_ID, NAME, DESCRIPTION, LIST_PRICE)
+    cursor.execute(command + values)
+    db.commit()
+
+
+def crear_ac_tenant(ID, NAME):
+    command = "INSERT INTO ac_tenant(ID, NAME) "
+    values = "VALUES ({}, {});".format(ID, NAME)
+    cursor.execute(command + values)
+    db.commit()
+
+def crear_customer(ID, TENANT_ID, FIRST_NAME, LAST_NAME):
+    command = "INSERT INTO customer(ID, TENANT_ID, FIRST_NAME, LAST_NAME)) "
+    values = "VALUES ({}, {}, {}, {});".format(ID, TENANT_ID, FIRST_NAME, LAST_NAME))
+    cursor.execute(command + values)
+    db.commit()
