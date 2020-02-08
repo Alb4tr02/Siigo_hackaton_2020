cb17c35a7ade493a2538710bc26250616894586c
Jimmer942 <942@holbertonschool.com>
2020-02-08 15:55:07 -0500

    update.py [update]

---
+++ metodos/update.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import MySQLdb
+
+
+def update_table(ID, TABLE, PARAMS):
+    db = MySQLdb.connect(host="localhost", user="electros", passwd="electros", db="siigo")
+    cursor = db.cursor()
+    st = str(PARAMS)
+    st = st.replace(",", "")
+    l = []
+    st = st.split(" ")
+    for i in st:
+        l.append(i.split("="))
+    dic = {}
+    for i in l:
+        dic[i[0]] = i[1]
+        for key, val in dic.items():
+            command = "UPDATE {} SET {}='{}'  WHERE id = {};".format(TABLE, key, val, ID)
+            cursor.execute(command)
+    db.commit()
+    db.close()
