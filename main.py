601713abb8782019c110bfa10597384ad0c1d72a
Alb4tr02 <834@holbertonschool.com>
2020-02-08 19:09:38 +0000

    main.py [update] new test for lists

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 
-from metodos.lista import listar
+from metodos.lista import list_table_id
 from metodos.crear import crear
 
 
-listar()
+
 print()
-
-crear()
-
-listar()
+list_table_id()
+list_table_id("customer", "")
