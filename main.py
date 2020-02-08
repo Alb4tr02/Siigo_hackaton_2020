bc61ad0b6bdc9e4ffd2f7f4596a0522ab9426b1a
Jimmer942 <942@holbertonschool.com>
2020-02-08 14:32:55 -0500

    main.py [Update: print the tables]

--- main.py
+++ main.py
@@ -3,11 +3,5 @@ from metodos.lista import *
 from metodos.crear import *
 from metodos.delete import *
 
-print()
 list_table_id()
-crear_ac_tenant(1, "prueba")
-crear_customer(1, 1, 'jimmer', 'hernandez')
-crear_ac_invoices(1, 1, 1, "abril", "2020", 1, 1, 1)
-crear_ac_products(1, 1, "papel", "una hoja de papel", 1)
-crear_ac_invoice_items(1, 1, 1, 1, 1, 1, 1)
 list_table_id()
