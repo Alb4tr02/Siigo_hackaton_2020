9896b1322d5726c636651060924f6b7ea5868066
Alb4Tr02 <834@holbertonschool.com>
2020-02-09 14:52:25 -0500

    test_api.sh [update] add creation of tenants

--- test_api.sh
+++ test_api.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-for i in `seq 10 1 200`
+for j in `seq 1 1 100`
+do
+    curl -d "id=$j&name=nata$j" -H "Content-Type: application/x-www-form-urlencoded" -X POST http://localhost:5000/api/v1/ac_tenant
+done
+for i in `seq 1 1 600`
 do
     curl -d "id=$i&tenant_id=2&name=papas_saladas&description=lol probando probando 123&list_price=1234.5" -H "Content-Type: application/x-www-form-urlencoded" -X POST http://localhost:5000/api/v1/ac_products
 done
