b374712231b0c9a64a1b5dd294fe146ebfa4f987
Alb4Tr02 <834@holbertonschool.com>
2020-02-08 21:58:05 -0500

    metodos/lista.py [fix] remove old functions and create only one to list whole table using the table name, and another one to filter a row by id, and another one to get the names of the columns in each table

--- metodos/lista.py
+++ metodos/lista.py
@@ -1,33 +1,43 @@
 #!/usr/bin/env python3
 
 import MySQLdb
-
-
-def list_table_id(table="*", id=""):
-       params = ["id", "tenant_id", "name", "description", "list_price"]
+def get_params(table_name):
        db = MySQLdb.connect(host="localhost", user="electros", passwd="electros", db="siigo")
        cursor = db.cursor()
-       query = "SELECT * FROM " + table
-       if (table == "*"):
-              query = "SELECT TABLE_NAME FROM  INFORMATION_SCHEMA.tables WHERE TABLE_SCHEMA='siigo';"
-              table = cursor.execute(query)
-              tables = []
-              for row in range(table):
-                     table_name = list(cursor.fetchone())
-                     tables.append(str(table_name[0]))
-              for name in tables:
-                     list_table_id(name, "")
-       else:
-              if (len(id) != 0):
-                     query = "SELECT * FROM " + table + " WHERE id = " + id
-              table = cursor.execute(query)
-              total = {}
-              for row in range(table):
-                     response = {}
-                     i = 0
-                     aux = list(cursor.fetchone())
-                     for value in aux:
-                            response[params[i]] = aux[i]
-                            i = i +  1
+       table = cursor.execute("SHOW COLUMNS FROM " + table_name)
+       params = []
+       for row in range(table):
+              aux = list(cursor.fetchone())
+              params.append(str(aux[0]))
+       return params
+
+def list_all_table(table_name):
+       db = MySQLdb.connect(host="localhost", user="electros", passwd="electros", db="siigo")
+       cursor = db.cursor()
+       query = "SELECT * FROM " + table_name
+       total = {}
+       params = get_params(table_name)
+       table = cursor.execute(query)
+       for row in range(table):
+              response = {}
+              i = 0
+              aux = list(cursor.fetchone())
+              for value in aux:
+                     response[params[i]] = aux[i]
+                     i = i +  1
                      total[aux[0]] = response
-              return total
+       return total
+
+def list_table_id(table_name, id):
+       params = get_params(table_name)
+       db = MySQLdb.connect(host="localhost", user="electros", passwd="electros", db="siigo")
+       cursor = db.cursor()
+       query = "SELECT * FROM " + table_name + " WHERE id = " + id
+       table = cursor.execute(query)
+       response = {}
+       aux = list(cursor.fetchone())
+       i = 0
+       for value in aux:
+              response[params[i]] = aux[i]
+              i = i +  1
+       return response
