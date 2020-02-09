09e07019860f3566e9a5703014394b17003ac684
Alb4Tr02 <834@holbertonschool.com>
2020-02-08 21:55:03 -0500

    api.py [update] add get method for whole table in all tables

--- api.py
+++ api.py
@@ -1,21 +1,27 @@
 from flask import Flask
 from flask import jsonify
+from flask import request
 from metodos.lista import *
+from metodos.crear import *
 
 app = Flask(__name__)
 
-@app.route('/api/v1/products', methods=['GET'])
-def get_products():
-    response = list_table_id("ac_products")
+@app.route('/api/v1/<table_name>', methods=['GET'])
+def get_table(table_name):
+    response = list_all_table(table_name)
     return jsonify(response)
 
-@app.route('/api/v1/products/<id>', methods=['GET'])
-def get_product(id):
-    response = {'message': 'success'}
+@app.route('/api/v1/<table_name>/id=<id>', methods=['GET'])
+def get_row_id(table_name, id):
+    response = list_table_id(table_name, id)
     return jsonify(response)
 
-@app.route('/api/v1/products/', methods=['POST'])
+
+@app.route('/api/v1/products', methods=['POST'])
 def create_product():
+    data = request.form
+    #falta crear funcion para verificar la validez de las claves primarias y foraneas
+    crear_ac_products(data)
     response = {'message': 'success'}
     return jsonify(response)
 
@@ -31,4 +37,3 @@ def delete_product(id):
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 5000, debug=True)
-    
