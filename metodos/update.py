3daf1f754de6707b2470f144c1c98c3dd488a090
Alb4Tr02 <834@holbertonschool.com>
2020-02-09 15:58:42 -0500

    metodos/update.py [update] change prototype to hable PUT request

--- metodos/update.py
+++ metodos/update.py
@@ -3,25 +3,16 @@
 import MySQLdb
 
 
-def update_table(ID, TABLE, PARAMS):
+def update_table(table_name, id, data):
     db = MySQLdb.connect(host="localhost", user="electros", passwd="electros", db="siigo")
     cursor = db.cursor()
     try:
-        st = str(PARAMS)
-        st = st.replace(",", "")
-        l = []
-        st = st.split(" ")
-        for i in st:
-            l.append(i.split("="))
-        dic = {}
-        for i in l:
-            dic[i[0]] = i[1]
-            for key, val in dic.items():
-                command = "UPDATE {} SET {}='{}'  WHERE id = {};".format(TABLE, key, val, ID)
-                cursor.execute(command)
+        for key, val in data.items():
+            command = "UPDATE {} SET {}='{}'  WHERE {};".format(table_name, key, val, id)
+            cursor.execute(command)
         db.commit()
         db.close()
-        return {'message': 'Update {} in table {} with {}'.format(ID, TABLE, PARAMS)}
+        return {'message': 'Update {} in table {} with {}'.format(id, table_name, data)}
     except:
         db.close()
         return {'error': 'Can not update an unexisting id'}
