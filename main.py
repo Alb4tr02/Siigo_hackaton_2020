efc2ddba173fe52b1636ac097a1d2c9388955dd7
Alb4Tr02 <834@holbertonschool.com>
2020-02-08 21:53:56 -0500

    main.py [update] update the funcions name

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from metodos.delete import *
 from metodos.update import *
 
 print("todos")
-list_table_id()
+list_all_table("ac_products")
 print("--------------------------------------------------")
 """crear_ac_tenant(1, "prueba")
 crear_customer(1, 1, 'jimmer', 'hernandez')
@@ -13,7 +13,7 @@ crear_ac_invoices(1, 1, 1, "abril", "2020", 1, 1, 1)
 crear_ac_products(1, 1, "papel", "una hoja de papel", 1)
 crear_ac_invoice_items(1, 1, 1, 1, 1, 1, 1)"""
 print("customer id = 1")
-list_table_id("customer", "1")
+list_table_id("ac_products", "1")
 print("--------------------------------------------------")
 print("Todos")
 list_table_id()
